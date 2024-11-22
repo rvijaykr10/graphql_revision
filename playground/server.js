b4065d7f45a72dbd95cab8f88fa934f16aa07894
vijay <vijay@vijays-MacBook-Pro.local>
2024-11-22 23:16:00 +0530

    basic graphql code added

---
+++ playground/server.js
@@ -0,0 +1,25 @@
+import { ApolloServer} from '@apollo/server'
+import { startStandaloneServer} from '@apollo/server/standalone'
+
+const typeDefs = `#graphql
+    # by default it will add
+    # schema {
+    #     query: Query
+    # }
+
+    type Query {
+        greeting: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello World'
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers})
+
+const info = await startStandaloneServer(server, {listen: {port:9000}})
+
+console.log(`Server running at ${info.url}`)
